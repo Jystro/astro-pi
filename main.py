e883fba957ff8efc29684982fe131d566d68ed1b
Jystro <35638179+Jystro@users.noreply.github.com>
2023-02-22 14:54:40 +0100

    Camera first version

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
-import time, os
+import time
 
-running_time =time.time() + 60 * 180
+running_time = time.time() + 60 * 180
+
+import os
+from picamera import PiCamera
+
+camera = PiCamera(resolution=(2592, 1944))
+camera.start_preview()
+
+time.sleep(2)
+
+camera.iso = 120
+
+print(camera.exposure_speed)
+camera.shutter_speed = camera.exposure_speed
+camera.exposure_mode = 'off'
+
+tmp = camera.awb_gains
+camera.awb_mode = 'off'
+camera.awb_gains = tmp
 
 path = os.path.realpath(os.path.dirname(__file__))
+
+camera.capture('test.jpg')
 
 while time.time() < running_time:
 
@@ -19,4 +39,3 @@ while time.time() < running_time:
 
 	#take photo
 	#classify
-	pass
