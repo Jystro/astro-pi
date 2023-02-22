44d1de51bc42cb41afbd43662768001d9b5f4cf1
Jystro <35638179+Jystro@users.noreply.github.com>
2023-02-22 21:57:46 +0100

    Fully working version

--- main.py
+++ main.py
@@ -1,36 +1,47 @@
 import time
+from logzero import logger, logfile
 
 running_time = time.time() + 60 * 180
 
 import os
-from picamera import PiCamera
-from io import BytesIO
-from PIL import Image
 
-path = os.path.realpath(os.path.dirname(__file__))
+base = os.path.realpath(os.path.dirname(__file__))
+images_path = os.path.join(base, 'images')
+
+logfile(os.path.join(base, 'logs.log'))
+logger.info('Started timer')
+
+from picamera import PiCamera
+from PIL import Image
+from orbit import ISS
+
 
 #camera
+logger.info('Starting camera...')
 camera = PiCamera(resolution=(2592, 1944))
-camera.start_preview()
 
-time.sleep(2)
+time.sleep(1)
+logger.info('Done')
 
 camera.iso = 120
 
 camera.shutter_speed = camera.exposure_speed
 camera.exposure_mode = 'off'
 
-tmp = camera.awb_gains
-camera.awb_mode = 'off'
-camera.awb_gains = tmp
+def convert(angle):
+		sign, degrees, minutes, seconds = angle.signed_dms()
+		exif_angle = f'{degrees:.0f}/1,{minutes:.0f}/1,{seconds*10:.0f}/10'
+		return sign < 0, exif_angle
+
 
 #model
+logger.info('Loading model...')
 import tflite_runtime.interpreter as tflite
 import numpy as np
 
 class_names = ['Clouds', 'Land', 'Night', 'Sea']
 
-model_path = 'model.tflite'
+model_path = os.path.join(base, 'model.tflite')
 
 interpreter = tflite.Interpreter(model_path=model_path,
 	experimental_delegates=[
@@ -43,32 +54,48 @@ shape = (shape[1], shape[2])
 
 interpreter.allocate_tensors()
 
+logger.info('Done')
+
+if not os.path.exists(images_path):
+	logger.info('Creating images dir...')
+	os.makedirs(images_path)
+	logger.info('Done')
 
 
 
+logger.info('Starting experiment')
 while time.time() < running_time:
 
 	#check space
 	size = 0
-	for path, dirs, files in os.walk(path):
+	for path, dirs, files in os.walk(base):
 		for f in files:
 			fp = os.path.join(path, f)
 			size += os.path.getsize(fp)
 
-	if size >= 3_000_000_000:
-		print('Reached max size')
+	if size >= 2_950_000_000:
+		logger.warning('Reached max size')
 		break
 
+	#geo locations
+	point = ISS.coordinates()
+
+	south, exif_latitude = convert(point.latitude)
+	west, exif_longitude = convert(point.longitude)
+
+	camera.exif_tags['GPS.GPSLatitude'] = exif_latitude
+	camera.exif_tags['GPS.GPSLatitudeRef'] = "S" if south else "N"
+	camera.exif_tags['GPS.GPSLongitude'] = exif_longitude
+	camera.exif_tags['GPS.GPSLongitudeRef'] = "W" if west else "E"
+
 	#take photo
-	stream = BytesIO()
+	name = 'img_{}'.format(time.strftime('%H_%M_%S', time.gmtime()))
 
-	camera.capture(stream, format='jpeg')#img_{}.jpg'.format(time.strftime('%H_%M_%S', time.gmtime()))
+	camera.capture(os.path.join(images_path, name + '.jpg'))
 
-	stream.seek(0)
-	image = Image.open(stream)
 
 	#classify
-	model_img = image.convert('RGB').resize(shape)
+	model_img = Image.open(os.path.join(images_path, name + '.jpg')).convert('RGB').resize(shape)
 
 	interpreter.tensor(interpreter.get_input_details()[0]['index'])()[0][:, :] = model_img
 
@@ -76,9 +103,23 @@ while time.time() < running_time:
 
 	scores = np.squeeze(interpreter.get_tensor(interpreter.get_output_details()[0]['index']))
 
-	print(scores)
-	print("This image most likely belongs to {}."
-	.format(class_names[int(np.where(scores == max(scores))[0])]))
+	klass = class_names[int(np.where(scores == max(scores))[0])]
 
-	#wait
-	time.sleep(5)
+
+	logger.info(klass)
+
+	#check
+	if klass == 'Sea':
+		logger.info(f'Saved {name}')
+		time.sleep(1)
+	elif klass == 'Clouds':
+		logger.info(f'Saved {name}')
+		time.sleep(2)
+	elif klass == 'Night':
+		os.remove(os.path.join(images_path, name + '.jpg'))
+		time.sleep(10)
+	else:
+		os.remove(os.path.join(images_path, name + '.jpg'))
+		time.sleep(2)
+
+logger.warning('Time\'s over')
